401417bfd65e8f0fe0196b5985141d38479e8c48
Jason <jev4zs@virginia.edu>
2018-07-01 19:26:36 -0700

    added some routes for player info

--- elm-src/Models.elm
+++ elm-src/Models.elm
@@ -3,14 +3,22 @@ module Models exposing (..)
 import RemoteData exposing (WebData)
 
 
+type Route
+    = PlayersRoute
+    | PlayerRoute PlayerId
+    | NotFoundRoute
+
+
 type alias Model =
     { players : WebData (List Player)
+    , route : Route
     }
 
 
-initialModel : Model
-initialModel =
+initialModel : Route -> Model
+initialModel route =
     { players = RemoteData.Loading
+    , route = route
     }
 
 
